06b1cc958fbfffcf790c9f32ecb68928ff6f21a9
Yesidh <944@holbertonschool.com>
2020-07-22 08:52:39 -0500

    [Fix]: deleting condition when len of a list is equal to cero and delting condition to add cero at the end of the first for cycle

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -21,20 +21,9 @@ def canUnlockAll(boxes):
                     pass
                 else:
                     unlocked_box.append(boxes[i][j])
-        if len(boxes[i]) == 0:
-            unlocked_box.append(0)
-
-    if 0 in unlocked_box:
-        pass
-    else:
-        unlocked_box.append(0)
+    unlocked_box.append(0)
 
     if len(unlocked_box) == len(boxes):
         return True
     else:
         return False
-
-
-if __name__ == "__main__":
-    boxes = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
-    print(canUnlockAll(boxes))
