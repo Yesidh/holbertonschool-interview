8ca5bb17c13e362d2b2d412e45ca5115479c7917
Yesidh <944@holbertonschool.com>
2020-07-22 09:13:17 -0500

    [Fix]: my code de novo

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """
-check if all lockboxes can be access
+=================================
+only canUnlockAll function inside
+=================================
 """
 
 
 def canUnlockAll(boxes):
     """
-    main
+    function for check if all boxes can be open
+    into a list, return True if all can be open
+    and False else
     """
-    visited = []
+
+    unlocked_box = []
     for i in range(len(boxes)):
         for j in range(len(boxes[i])):
-            if i < boxes[i][j] < len(boxes):
-                visited.append(boxes[i][j])
-        if len(boxes[i]) == 0 and i == len(boxes) - 1:
-            visited.append(0)
-    if len(visited) == len(boxes):
+            if boxes[i][j] < len(boxes):
+                if boxes[i][j] in unlocked_box:
+                    pass
+                else:
+                    unlocked_box.append(boxes[i][j])
+
+    unlocked_box.append(0)
+
+    if len(unlocked_box) == len(boxes):
         return True
     else:
         return False
