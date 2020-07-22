4d148ec696f4dc2782b1b771b6eb2e27d9950e1d
Yesidh <944@holbertonschool.com>
2020-07-22 10:51:36 -0500

    [Fix]: changing indes by valor inside each list

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -13,18 +13,22 @@ def canUnlockAll(boxes):
     and False else
     """
 
-    unlocked_box = []
+    unlocked_box = [0]
     for i in range(len(boxes)):
-        for j in range(len(boxes[i])):
-            if boxes[i][j] < len(boxes):
-                if boxes[i][j] in unlocked_box:
+        for j in boxes[i]:
+            if j < len(boxes):
+                if j in unlocked_box:
                     pass
                 else:
-                    unlocked_box.append(boxes[i][j])
+                    unlocked_box.append(j)
 
-    unlocked_box.append(0)
+#    unlocked_box.append(0)
 
     if len(unlocked_box) == len(boxes):
         return True
     else:
         return False
+if __name__ == "__main__":
+#   boxes =  [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    boxes = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
+    print(canUnlockAll(boxes))
