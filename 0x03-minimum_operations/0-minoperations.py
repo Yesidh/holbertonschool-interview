aa318087da387d88549412d37a15be90c1d42950
Yesidh <944@holbertonschool.com>
2020-08-25 11:27:59 -0500

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -16,12 +16,13 @@ def minOperations(n):
         return 0
 
     copy_paste = 0
-    prime_factors = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 113]
+    prime_factors = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                     53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 113]
     i = 0
 
     while n > 1 and i <= 25:
         if n % prime_factors[i] == 0:
-            copy_paste += prime_factors[i] 
+            copy_paste += prime_factors[i]
             n /= prime_factors[i]
         else:
             i += 1
