4babacc1dc6b65c08bff3590fc9bca0a06ecd8a1
Yesidh <944@holbertonschool.com>
2020-09-02 17:12:53 -0500

    [fix]: changing pattern_size for any number of digits.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,19 +9,18 @@ import sys
 import re
 
 pattern_error = re.compile(r'\s(\d{3})\s')
-pattern_size = re.compile(r'\s(\d{3})$')
+pattern_size = re.compile(r'\s(\d+)$')
 text_to_search = sys.stdin.readline()
 files_size = 0
 ten_times = 0
 errors_code = [200, 301, 400, 401, 403, 404, 405, 500]
 errors_code_count = [0, 0, 0, 0, 0, 0, 0, 0]
 
-match_pattern_error = pattern_error.finditer(text_to_search)
-match_pattern_size = pattern_size.finditer(text_to_search)
-
-
 try:
-    while (match_pattern_error and match_pattern_size):
+    while (text_to_search):
+        match_pattern_error = pattern_error.finditer(text_to_search)
+        match_pattern_size = pattern_size.finditer(text_to_search)
+
         for match in match_pattern_error:
             if int(match.group(1)) in errors_code:
                 index_error = errors_code.index(int(match.group(1)))
@@ -31,8 +30,6 @@ try:
             files_size += int(match.group(1))
 
         text_to_search = sys.stdin.readline()
-        match_pattern_error = pattern_error.finditer(text_to_search)
-        match_pattern_size = pattern_size.finditer(text_to_search)
         ten_times += 1
 
         if (ten_times == 10):
