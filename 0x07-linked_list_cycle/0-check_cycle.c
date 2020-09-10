06468808e849e8b9fbdce0cf5ca8479bb9437f53
Yesidh <944@holbertonschool.com>
2020-09-09 23:01:59 -0500

    [deleting cycle variable]

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -8,10 +8,9 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *head, *prev;
-	int cycle = 0;
 
 	if (list == NULL || list->next == NULL)
-		return (cycle);
+		return (0);
 
 	head = prev = list;
 
@@ -20,10 +19,7 @@ int check_cycle(listint_t *list)
 		head = head->next;
 		prev = prev->next->next;
 		if (head == prev)
-		{
-			cycle = 1;
-			return (cycle);
-		}
+			return (1);
 	}
-	return (cycle);
+	return (0);
 }
