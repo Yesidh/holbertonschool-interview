83ce971b3ad033ba6f2da908a25f40e77597a150
Yesidh <944@holbertonschool.com>
2020-09-14 10:21:11 -0500

    [Feature]: adding header file

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,19 @@
+#ifndef PALIN_H
+#define PALIN_H
+
+# include<stdlib.h>
+# include<stdio.h>
+
+/* /\** */
+/*  * struct truple - a integer and an string */
+/*  * @n: integer */
+/*  * @str: string */
+/*  *\/ */
+/* typedef struct tuple */
+/* { */
+/* 	int n; */
+/* 	char str; */
+/* } tuple_r; */
+
+int is_palindrome(unsigned long n);
+#endif
