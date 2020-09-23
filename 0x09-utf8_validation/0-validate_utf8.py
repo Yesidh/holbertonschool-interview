971c0a536b17235c95f24ba4ab6a5a1762e6dbe6
Yesidh <944@holbertonschool.com>
2020-09-23 16:51:55 -0500

    [pep8]

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,8 @@ validUTF8 method
 
 def validUTF8(data):
     """
-    method that determines if a given data set represents a valid UTF-8 encoding
+    method that determines if a given data set represents a valid UTF-8
+    encoding
     """
 
     n_bytes = 0
