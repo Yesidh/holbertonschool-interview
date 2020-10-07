cd7ee8a91298c20e5756b3ecca45a9d686607264
Yesidh <944@holbertonschool.com>
2020-10-07 15:00:54 -0500

    [s 0 s]

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef _S_PILES_H
+#define _S_PILES_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void copy_grid(int grid2[3][3], int grid1[3][3]);
+int grid_unstable(int grid1[3][3]);
+void fill_0(int grid2[3][3]);
+
+#endif /* _S_PILES_H */
