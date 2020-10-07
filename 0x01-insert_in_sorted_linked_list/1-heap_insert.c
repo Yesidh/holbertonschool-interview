062febb32ece8457b5dfb1d2cee6718cd6c9ca1e
Yesidh <944@holbertonschool.com>
2020-10-07 12:20:26 -0500

    [s 0 s]

--- 0x01-insert_in_sorted_linked_list/1-heap_insert.c
+++ 0x01-insert_in_sorted_linked_list/1-heap_insert.c
@@ -2,7 +2,7 @@
 
 /**
  * heap_insert - Insert in the heap
- * @parent: (binary_tree_t *) Head of the node
+ * @root: (binary_tree_t *) Head of the node
  * @value: (int) Value to pass at the node
  *
  * Return: (binary_tree_t *) Address of the new node or NULL if it fail
